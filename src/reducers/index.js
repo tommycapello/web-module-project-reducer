a2bb1d9246d47ed95029aefe7a96d6afe17bc1ec
Thomas Capello <thomcapello@gmail.com>
2021-04-12 19:06:34 -0400

    MVP

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, MEMORY_RECORD, MEMORY_CLEAR, MEMORY_RECALL} from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -19,24 +19,38 @@ const calculateResult = (num1, num2, operation) => {
 
 const reducer = (state, action) => {
     switch(action.type) {
-        case(ADD_ONE):
-            return({
-                ...state,
-                total: state.total + 1
-            });
 
         case(APPLY_NUMBER):
-            return ({ 
-                ...state, 
+            return ({
+                ...state,
                 total: calculateResult(state.total, action.payload, state.operation)
             });
-        
+
         case(CHANGE_OPERATION):
             return ({
                 ...state,
                 operation: action.payload
             });
-            
+            case(CLEAR_DISPLAY):
+            return ({
+                ...state, total:0
+            });
+
+        case(MEMORY_RECORD):
+            return ({
+                ...state, memory: state.total,
+            });
+
+        case(MEMORY_CLEAR):
+        return ({
+            ...state, memory:0
+        });
+
+        case(MEMORY_RECALL):
+            return ({
+                ...state, total: calculateResult(state.total, state.memory, state.operation)
+            });
+
         default:
             return state;
     }
